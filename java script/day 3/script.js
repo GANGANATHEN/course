08a49b0e7addadc30387bf02465fca9ac2f69011
GREENGANGA <GreeN11.dev@gmail.com>
2024-07-27 11:39:54 +0530

    re edit

--- java script/day 3/script.js
+++ java script/day 3/script.js
@@ -10,18 +10,18 @@ document.getElementById('gradeForm').addEventListener('submit', function(event) 
     const totalMark =(m1+m2+m3+m4+m5)/5;
 
     let grade;
-    if(totalMark<=100){
-        grade = "A";
-    }else if(totalMark<=90){
-        grade = "B";
-    }else if(totalMark<=80){
-        grade = "C";
-    }else if(totalMark<=70){
-        grade = "D";
+    if(totalMark<=50){
+        grade = "Fail";
     }else if(totalMark<=60){
         grade = "E";
-    }else if(totalMark<=50){
-        grade = "Fail";
+    }else if(totalMark<=70){
+        grade = "D";
+    }else if(totalMark<=80){
+        grade = "C";
+    }else if(totalMark<=90){
+        grade = "B";
+    }else if(totalMark<=100){
+        grade = "A";
     }else{
         grade = "Pleace Enter Mark Correctly... foooooollll..."
     }
