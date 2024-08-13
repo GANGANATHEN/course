3f2a36653ffa6a255b0f84e5e6b82e1739005751
GREENGANGA <GreeN11.dev@gmail.com>
2024-08-13 19:40:45 +0530

    time add

---
+++ java script/day 7/script.js
@@ -0,0 +1,45 @@
+// const inputText = document.getElementById("ine").value
+// function validate(){
+//     let maformat = /^[a-zA-Z0-9._%+-]+@gmail\.com$/
+
+//     if(inputText == maformat){
+//         alert ("Valid email address")
+//     }
+//     else(
+//         alert ("Not Valid email address")
+//     )
+
+// }
+
+// const d = new Date();
+// const month =d.getMonth()+1;
+// const year =d.getFullYear();
+
+
+// const a = d.setMonth(12);
+// const y = d.setDate(2);
+// const all = new Date();
+// const d = getDate();
+// document.getElementById("date").innerText = d
+// console.log(month);
+// console.log(year);
+
+// const date = new Date();
+// document.getElementById("dat").innerText = date
+
+const date = new Date();
+let day = date.getDate();
+let month = date.getMonth()+1;
+let year = date.getFullYear();
+
+
+// console.log(date)
+// console.log(day)
+// console.log(month)
+// console.log(year)
+
+document.getElementById("dat").innerText = `${day}day/${month}/month${year}/year`
+
+var start = Date.now()
+console.log(start);
+
