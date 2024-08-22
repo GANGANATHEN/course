618fcdd6d9e0c89ee87e0ed6f8feb0fe3e30bedc
GREENGANGA <GreeN11.dev@gmail.com>
2024-08-22 16:47:00 +0530

    api added

---
+++ java script/day 8/script.js
@@ -0,0 +1,57 @@
+// const date = new Date();
+// const Time = date.getFullYear();
+// const Month = date.getMonth()+1
+// const Year = date.getFullYear()
+// const Day = date.getDay()
+
+// const date = new Date().getDate();
+// console.log(date);
+// console.log(Day);
+
+// document.getElementById("ds").innerText = `Today Date ${Day}/${Month}/${Year}`
+
+
+
+
+
+//   var g= fetch("https://api.github.com/users/arun-cloud-dev")
+//   console.log(g);
+  
+
+
+//   .then(response =>{
+//     throw new Error("Warning..!");  
+//   }return response.json())
+// fetch('https://api.github.com/users/arun-cloud-dev')
+// .then(response =>{
+//     if(!response.ok){
+//         throw new Error("Warning..!"+response);
+//     }
+//     return response.json()
+// })
+
+// .then(data =>{
+//     console.log("The user is",data.login);
+// })
+
+// .catch(error =>{
+//     console.error("Error...!")
+// })
+fetch('https://api.github.com/users/arun-cloud-dev')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Warning..! " + response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log("Full API data:", data);
+        console.log("User ID:", data.id);
+        console.log("Username:", data.login);
+        console.log("Followers:", data.followers);
+        document.getElementById("ds").innerText = data.name
+    })
+    .catch(error => {
+        console.error("Error:", error.message);
+    });
+
