d2564c074dfd21abc26fc50cb457fb299b7e46d5
GREENGANGA <GreeN11.dev@gmail.com>
2024-09-09 13:38:35 +0530

    task2-changed

--- Front-end/html-css-js/tasks/task-2/script.js
+++ Front-end/html-css-js/tasks/task-2/script.js
@@ -6,13 +6,12 @@ const score2 = document.getElementById("ds-2");
 const tbtn1 = document.getElementById("Tbtn-1");
 const tbtn2 = document.getElementById("Tbtn-2");
 
+//remaining balls
+const reball = document.getElementById("remaining-balls");
+
 // bat or bowl
 const dp1 = document.getElementById("dp-1");
 const dp2 = document.getElementById("dp-2");
-
-// ballcount
-const bcou = document.getElementById("b-count").value;
-
 
 // buttons ...
 
@@ -24,6 +23,49 @@ const but3 = document.getElementById("btn-3");
 const but4 = document.getElementById("btn-4");
 const but5 = document.getElementById("btn-5");
 const but6 = document.getElementById("btn-6");
+
+//finish button
+const fbtn1 = document.getElementById("btn-t1")
+const fbtn2 = document.getElementById("btn-t2")
+
+//total score
+const total1 = document.querySelector(".total1");
+const total2 = document.querySelector(".total2");
+
+// toss
+
+let balls = 0;
+
+function toss() {
+    // Randomly decide if Team 1 or Team 2 wins the toss
+    const isTeam1Win = Math.random() < 0.5;
+    // ballcount
+    balls =document.getElementById("b-count").value;
+
+    if (isTeam1Win) {
+        document.getElementById('dp-1').textContent = "Team 1 wins the toss.";
+        tbtn1.style.display = 'block';
+        dp2.innerText = "Bowl";
+        document.getElementById('tos').style.display = 'none';
+        document.querySelector('.team-1').style.display = 'block';
+        document.getElementById('b-show').innerText = `Total balls = ${balls}`;
+        reBalls()
+    } else {
+        reBalls()
+        document.getElementById('dp-2').textContent = "Team 2 wins the toss.";
+        tbtn2.style.display = 'block';
+        dp1.innerText = "Bowl";
+        document.getElementById('tos').style.display = 'none';
+        document.querySelector('.team-2').style.display = 'block';
+        document.getElementById('b-show1').innerText = `Total balls = ${balls}`;
+    }
+}
+
+
+
+function reBalls(){
+    reball.innerText = `remaining balls = ${balls}`
+}
 
 let count1 = 0;
 let count2 = 0;
@@ -37,18 +79,24 @@ function butt1(){
     t1ballcount +=1;
     score1.innerText = "Score = " + count1;
     document.getElementById('show').innerText ="ballcount = "+ t1ballcount;
+    balls-=1;
+    reBalls()
 }
 function butt2(){
     count1 +=4;
     score1.innerText = "Score = " + count1;
     t1ballcount +=1;
     document.getElementById('show').innerText ="ballcount = "+ t1ballcount;
+    balls-=1;
+    reBalls()
 }
 function butt3(){
     count1 +=6;
     score1.innerText = "Score = " + count1;
     t1ballcount +=1;
     document.getElementById('show').innerText ="ballcount = "+ t1ballcount;
+    balls-=1;
+    reBalls()
 }
 
 // Team-2 buttons
@@ -58,39 +106,48 @@ function butt4(){
     t2ballcount +=1;
     score2.innerText = "Score = " + count2;
     document.getElementById('show1').innerText ="ballcount = "+ t2ballcount;
+    balls-=1;
+    reBalls()
 }
 function butt5(){
     count2 +=4;
     t2ballcount +=1;
     score2.innerText = "Score = " + count2;
     document.getElementById('show1').innerText ="ballcount = "+ t2ballcount;
+    balls-=1;
+    reBalls()
 }
 function butt6(){
     count2 +=6;
     t2ballcount +=1;
     score2.innerText = "Score = " + count2;
     document.getElementById('show1').innerText ="ballcount = "+ t2ballcount;
+    balls-=1;
+    reBalls()
 }
 
-// toss 
 
 
+//Team1 finsh button
 
-function toss() {
-    // Randomly decide if Team 1 or Team 2 wins the toss
-    const isTeam1Win = Math.random() < 0.5;
-
-    if (isTeam1Win) {
-        document.getElementById('dp-1').textContent = "Team 1 wins the toss.";
-        tbtn1.style.display = 'block';
-        dp2.innerText = "Bowl";
-        document.getElementById('tos').style.display = 'none';
-        document.getElementById('b-show').innerText = `Total balls = ${bcou}`;
-    } else {
-        document.getElementById('dp-2').textContent = "Team 2 wins the toss.";
-        tbtn2.style.display = 'block';
-        dp1.innerText = "Bowl";
-        document.getElementById('tos').style.display = 'none';
-        document.getElementById('b-show1').innerText = `Total balls = ${bcou}`;
+fbtn1.addEventListener("click",()=>{
+    console.log("im worth")
+    if(balls==t1ballcount){
+        total1.innerText = `Total runs = ${count1}`
     }
-}
+    else{
+        total1.innerText = `Total runs = ${count1}`
+    }
+})
+
+//Team2 finsh button
+
+fbtn2.addEventListener("click",()=>{
+    console.log("im worth")
+    if(balls==t2ballcount){
+        total2.innerText = `Total runs = ${count2}`
+    }
+    else{
+        total2.innerText = `Total runs = ${count2}`
+    }
+})
